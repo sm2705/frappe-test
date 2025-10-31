9b9cc177d11843a507b777257c003f2efccbfceb
sam <jsamrajadurai77@gmail.com>
2025-10-31 13:12:24 +0530

    Embed apps/estimator as normal files

---
+++ apps/estimator/estimator/estimator/page/estimator_welcomepag/estimator_welcomepag.js
@@ -0,0 +1,7 @@
+frappe.pages['estimator-welcomepag'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Estimator',
+		single_column: true
+	});
+}
